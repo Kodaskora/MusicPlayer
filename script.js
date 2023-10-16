44cc6a178f3684d76eb6361788dc3f58b40d48b9
Kodaskora <tomas.kutniauskis@gmail.com>
2023-10-16 14:23:34 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const musicContainer = document.getElementById('music-container');
+const audio = document.getElementById('audio');
+const cover = document.getElementById('cover');
+const title = document.getElementById('title');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+const prevBtn = document.getElementById('prev');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+
+
+play.addEventListener('click', () => audio.play())
