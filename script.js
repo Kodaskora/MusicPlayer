79ceedcfa31e7cd8394296bcd5e502486839edd8
Kodaskora <tomas.kutniauskis@gmail.com>
2023-10-16 19:29:37 +0200

    Js

--- script.js
+++ script.js
@@ -8,5 +8,84 @@ const prevBtn = document.getElementById('prev');
 const progress = document.getElementById('progress');
 const progressContainer = document.getElementById('progress-container');
 
+const songs = ['coldplay', 'gladiator', 'rihanna'];
 
-play.addEventListener('click', () => audio.play())
+let songIndex = 0;
+
+loadSong(songs[songIndex]);
+
+function loadSong(song) {
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+  title.innerText = song;
+}
+
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+  audio.play();
+}
+
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+  audio.pause();
+}
+
+function nextSong() {
+  songIndex++;
+
+  if (songIndex >= songs.length) {
+    songIndex = 0;
+  }
+  
+  loadSong(songs[songIndex]);
+
+  playSong();
+  
+}
+
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+  
+}
+
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clicX = e.offsetX;
+  const duration = audio.duration;
+  audio.currentTime = (clicX / width) * duration;
+}
+
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+nextBtn.addEventListener('click', nextSong);
+
+prevBtn.addEventListener('click', prevSong);
+
+audio.addEventListener('timeupdate', updateProgress);
+
+progressContainer.addEventListener('click', setProgress);
